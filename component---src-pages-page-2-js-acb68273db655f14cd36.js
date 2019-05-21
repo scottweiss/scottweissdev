c25d2f61aefa9edf5b04a8c87ad4d2ee7c8fdee6
Scott Weiss <scott@temboinc.com>
2019-05-21 16:15:30 -0700

    Updates

---
+++ component---src-pages-page-2-js-acb68273db655f14cd36.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{155:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(56),c=a(158),r=a(157);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-acb68273db655f14cd36.js.map
---
+++ styles-ee8396f27f18aa5955f6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{135:function(n,w,o){}}]);
+//# sourceMappingURL=styles-ee8396f27f18aa5955f6.js.map
